76749d94a07319f552614371686bf31b8cf2d1df
= <owen.marshall1@ontariotechu.net>
2026-04-01 22:32:20 -0400

    redid main menu screen

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,11 +1,17 @@
 extends VBoxContainer
 
+@onready var scene_transistion = $"../scene_transition/AnimationPlayer"
+
 const GRAVEYARD = preload("res://scenes/Graveyard.tscn")
 func _ready():
 	$start.grab_focus()
 	
 func _on_start_button_pressed():
+	scene_transistion.play("dissolve")
+	await scene_transistion.animation_finished
+	scene_transistion.play_backwards("dissolve")
 	get_tree().change_scene_to_packed(GRAVEYARD)
+	
 	
 func _on_quit_pressed():
 	get_tree().quit()
